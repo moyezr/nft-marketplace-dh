8676e8adcf70ec53dd90b3b503d4fd4ac55baec8
Moyez <moyezrabbani@gmail.com>
2023-01-07 16:18:51 +0530

    Added Services and Big NFT Slider Sections to the Home page

---
+++ client/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Style from "./Subscribe.module.css";
+
+const Subscribe = () => {
+  return (
+    <div>Subscribe</div>
+  )
+}
+
+export default Subscribe
---
+++ client/components/componentsindex.js
@@ -0,0 +1,9 @@
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+import Subscribe from "./Subscribe/Subscribe";
+
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Subscribe };
